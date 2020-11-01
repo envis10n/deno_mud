9935074ee5efe2c2452972a98359ba602e02649f
Envis10n <envis10n@protonmail.com>
2020-10-31 20:24:35 -0500

    Plugin api update

--- src/plugin_api.ts
+++ src/plugin_api.ts
@@ -1,8 +1,9 @@
 export {TcpClient, TcpServer} from "./net/tcp.ts";
 export { Database, Collection } from "./storage.ts";
-import { ICollection as _ICollection } from "./storage.ts";
+import { ICollection as _ICollection, IDocument as _IDocument } from "./storage.ts";
 import { IContext as _IContext, IPlugin as _IPlugin} from "./plugins.ts";
 
 export type ICollection = _ICollection;
 export type IContext = _IContext;
 export type IPlugin = _IPlugin;
+export type IDocument = _IDocument;
